9588927ee7a4aa417b4e88856e7f119f11067ced
mhenriquecode <matheucar186@gmail.com>
2025-09-03 17:41:22 -0300

    feat: implementing product for query

---
+++ query-service/src/main/java/org/cqrs/entity/Product.java
@@ -0,0 +1,21 @@
+package org.cqrs.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "PRODUCT_QUERY")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    @Id
+    private long id;
+    private String name;
+    private String description;
+    private double price;
+}
