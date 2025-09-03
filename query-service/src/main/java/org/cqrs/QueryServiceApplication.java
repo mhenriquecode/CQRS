07de5ca9a453a6123a1f1956cfea1e12d60fea17
mhenriquecode <matheucar186@gmail.com>
2025-09-03 17:45:26 -0300

    chore: reorganize project folder structure

--- query-service/src/main/java/org/cqrs/QueryServiceApplication.java
+++ query-service/src/main/java/org/cqrs/QueryServiceApplication.java
@@ -1,4 +1,4 @@
-package org.cqrs.queryservice;
+package org.cqrs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- query-service/src/test/java/org/cqrs/QueryServiceApplicationTests.java
+++ query-service/src/test/java/org/cqrs/QueryServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package org.cqrs.queryservice;
+package org.cqrs;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
