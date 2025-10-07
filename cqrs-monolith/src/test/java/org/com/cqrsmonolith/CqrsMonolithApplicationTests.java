b331f8828fc9dd27264558a4990c878b4a4eb3a2
mhenriquecode <matheucar186@gmail.com>
2025-10-07 19:09:19 -0300

    Adding CQRS monolith package

---
+++ cqrs-monolith/src/test/java/org/com/cqrsmonolith/CqrsMonolithApplicationTests.java
@@ -0,0 +1,13 @@
+package org.com.cqrsmonolith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CqrsMonolithApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
